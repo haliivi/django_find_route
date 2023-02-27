0c3e43488ef0b26bd2f580c23441bd5d719126e2
a.sotnichenko <haliivi@mail.ru>
2023-02-27 16:49:21 +0700

    1) Made migrations and apply it. 2) Added 3 models. 3) Plug in applications trains.

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from cities.models import *
+__all__ = [
+    'Train',
+]
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Базовый класс модели
+    """
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Train(BaseModel):
+    name = models.CharField(max_length=20, unique=True, verbose_name='Номер поезда')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
+    from_city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        # null=True,
+        # blank=True,
+        related_name='from_city_set',
+        verbose_name='Из какого города',
+    )
+    to_city = models.ForeignKey(
+        'cities.City',
+        on_delete=models.CASCADE,
+        # null=True,
+        # blank=True,
+        related_name='to_city_set',
+        verbose_name='В какой город',
+    )
+
+    def __str__(self):
+        return f'Поезд № {self.name} из города {self.from_city}'
+
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = [
+            'travel_time',
+        ]
+
+
+class TrainTest(BaseModel):
+    name = models.CharField(max_length=20, unique=True, verbose_name='Номер поезда')
+    from_city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        # null=True,
+        # blank=True,
+        related_name='from_city',
+        verbose_name='Из какого города',
+    )
