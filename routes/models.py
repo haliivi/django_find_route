1c79e4faf7b66c1c9e9cb4a2b65fd564ea59e34e
a.sotnichenko <haliivi@mail.ru>
2023-02-28 10:32:59 +0700

    1) Added model Route. 2) Made migrations and apply it.

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from cities.models import *
+__all__ = [
+    'Route'
+]
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Базовый класс модели
+    """
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Route(BaseModel):
+    name = models.CharField(max_length=20, unique=True, verbose_name='Название маршрута')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Общее время в пути')
+    from_city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        related_name='route_from_city_set',
+        verbose_name='Из какого города',
+    )
+    to_city = models.ForeignKey(
+        'cities.City',
+        on_delete=models.CASCADE,
+        related_name='route_to_city_set',
+        verbose_name='В какой город',
+    )
+    trains = models.ManyToManyField(
+        'trains.Train',
+        verbose_name='Список поездов'
+    )
+
+    def __str__(self):
+        return f'Маршрут {self.name} из города {self.from_city}'
+
+    class Meta:
+        verbose_name = 'Маршрут'
+        verbose_name_plural = 'Маршруты'
+        ordering = [
+            'travel_time',
+        ]
