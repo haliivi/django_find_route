c07f8fcc599afe762f4849a8a2d95ffcb6aee743
a.sotnichenko <haliivi@mail.ru>
2023-02-28 10:10:00 +0700

    1) Added to base.html 2 dropdown items. 2) Plugin all class view to path pattern. 3) Refactoring templates. 4) Create forms.py and write class TrainForm.

--- trains/urls.py
+++ trains/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', TrainListView.as_view(), name='home'),
     path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),
-    # path('add/', TrainCreateView.as_view(), name='create'),
-    # path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
-    # path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
+    path('add/', TrainCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
 ]
--- trains/views.py
+++ trains/views.py
@@ -1,17 +1,16 @@
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
-from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 from .models import *
+from .forms import *
 __all__ = [
     'TrainListView',
     'TrainDetailView',
-    # 'TrainCreateView',
-    # 'TrainUpdateView',
-    # 'TrainDeleteView',
+    'TrainCreateView',
+    'TrainUpdateView',
+    'TrainDeleteView',
 ]
 
 
@@ -26,29 +25,28 @@ class TrainDetailView(DetailView):
     template_name = 'trains/detail.html'
 
 
-# class TrainCreateView(SuccessMessageMixin, CreateView):
-#     model = Train
-#     form_class = TrainForm
-#     template_name = 'cities/create.html'
-#     success_url = reverse_lazy('cities:home')
-#     success_message = 'Город успешно создан'
-#
-#
-# class TrainUpdateView(SuccessMessageMixin, UpdateView):
-#     model = Train
-#     form_class = TrainForm
-#     template_name = 'cities/update.html'
-#     success_url = reverse_lazy('cities:home')
-#     success_message = 'Город успешно отредактирован'
-#
-#
-# class TrainDeleteView(DeleteView):
-#     model = Train
-#     # template_name = 'cities/delete.html'
-#     success_url = reverse_lazy('cities:home')
-#
-#     def get(self, request, *args, **kwargs):
-#         messages.success(request, 'Город успешно удален')
-#         self.object = self.get_object()
-#         self.object.delete()
-#         return HttpResponseRedirect(self.get_success_url())
+class TrainCreateView(SuccessMessageMixin, CreateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/create.html'
+    success_url = reverse_lazy('trains:home')
+    success_message = 'Поезд успешно создан'
+
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/update.html'
+    success_url = reverse_lazy('trains:home')
+    success_message = 'Поезд успешно отредактирован'
+
+
+class TrainDeleteView(DeleteView):
+    model = Train
+    success_url = reverse_lazy('trains:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Поезд успешно удален')
+        self.object = self.get_object()
+        self.object.delete()
+        return HttpResponseRedirect(self.get_success_url())
